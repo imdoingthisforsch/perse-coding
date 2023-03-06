2e7d8b51e082830fbd5896c9dfd48c04a58143f2
imdoingthisforsch <125532706+imdoingthisforsch@users.noreply.github.com>
2023-03-06 10:16:59 +0800

    Update and rename idk to task1.py

---
+++ specimenp2/task1.py
@@ -0,0 +1,9 @@
+x = input("please input your 1st number here: ")
+y = input("please input your 2nd number here: ")
+z = input("please input your 2nd number here: ")
+if z > y > x:
+  print('UP')
+elif x > y > z:
+  print('DOWN')
+else:
+  print('WOBBLY')
