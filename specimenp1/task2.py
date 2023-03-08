aee7fbc4eb87f0ff942cd11b7739947de1418fe8
imdoingthisforsch <125532706+imdoingthisforsch@users.noreply.github.com>
2023-03-08 22:08:21 +0800

    Create task2.py

---
+++ specimenp1/task2.py
@@ -0,0 +1,8 @@
+x = int(input('input any number in here: '))
+y = int(input('input any different number in here: '))
+if x > y:
+  print(x - y)
+elif x < y:
+  print(y - x)
+else: 
+  print("your 2 numbers are the same!")
